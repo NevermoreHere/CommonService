81a6e6dcdee2b57bd4514a8193bb0c4423115205
NervermoreHere <xulide12345@gmail.com>
2021-12-29 19:00:22 +0800

    add method

--- common/src/main/java/com/company/common/constant/DefaultOrg.java
+++ common/src/main/java/com/company/common/constant/DefaultOrg.java
@@ -13,4 +13,31 @@ public enum DefaultOrg {
         this.role = role;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getRole() {
+        return role;
+    }
+
+    public void setRole(long role) {
+        this.role = role;
+    }
+
+    public long getId(DefaultOrg org){
+        return org.getId();
+    }
 }
--- common/src/main/java/com/company/common/constant/DefaultRole.java
+++ common/src/main/java/com/company/common/constant/DefaultRole.java
@@ -1,5 +1,7 @@
 package com.company.common.constant;
 
+import lombok.Data;
+
 public enum DefaultRole {
     NORMAL(1, "普通用户", "[1]"),
     VIP(2, "vip用户","[1,2,3]");
@@ -11,5 +13,33 @@ public enum DefaultRole {
         this.id = id;
         this.name = name;
         this.authList = authList;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthList() {
+        return authList;
+    }
+
+    public void setAuthList(String authList) {
+        this.authList = authList;
+    }
+
+    public long getId(DefaultRole role){
+        return role.getId();
     }
 }
