4e505277d1c5970b4d4cc22cdcba6d12cde62c2c
NervermoreHere <xulide12345@gmail.com>
2021-12-29 18:55:22 +0800

    添加默认的角色和组织

--- common/src/main/java/com/company/common/constant/DefaultOrg.java
+++ common/src/main/java/com/company/common/constant/DefaultOrg.java
@@ -1,13 +1,13 @@
 package com.company.common.constant;
 
-public enum  Org {
+public enum DefaultOrg {
     NORMAL(1, "普通用户组", 1),
     VIP(2, "vip用户组",2);
     private long id;
     private String name;
     private long role;
 
-    Org(long id, String name, long role){
+    DefaultOrg(long id, String name, long role){
         this.id = id;
         this.name = name;
         this.role = role;
--- common/src/main/java/com/company/common/constant/DefaultRole.java
+++ common/src/main/java/com/company/common/constant/DefaultRole.java
@@ -1,13 +1,13 @@
 package com.company.common.constant;
 
-public enum  Role {
+public enum DefaultRole {
     NORMAL(1, "普通用户", "[1]"),
     VIP(2, "vip用户","[1,2,3]");
     private long id;
     private String name;
     private String authList;
 
-    Role(long id, String name, String  authList){
+    DefaultRole(long id, String name, String  authList){
         this.id = id;
         this.name = name;
         this.authList = authList;
